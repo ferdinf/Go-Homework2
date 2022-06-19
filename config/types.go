a45368c3ac38583f4ee89d37f90a726e344c3f9c
ferdinf <ferdinfaizi31@gmail.com>
2022-06-19 22:00:55 +0700

    creating types.go file

--- config/types.go
+++ config/types.go
@@ -1,1 +1,13 @@
 package config
+
+type Configuration struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+	DB   DB
+}
+
+type DB struct {
+	User string `json:"ferdin"`
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
