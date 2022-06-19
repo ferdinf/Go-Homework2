a0ee0f5442b3fd3bc3b198d84ce0e577cd05fac1
ferdinf <ferdinfaizi31@gmail.com>
2022-06-19 22:14:28 +0700

    creating config.go file and import gonfig

--- config/config.go
+++ config/config.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"fmt"
+	"github.com/tkanos/gonfig"
+	"os"
+)
+
+func GetConfig() Configuration {
+	configuration := Configuration{}
+	err := gonfig.GetConf(getFileName(), &configuration)
+	if err != nil {
+		fmt.Println(err, "failed to read configuration")
+		os.Exit(1)
+	}
+	return configuration
+}
+
+func getFileName() string {
+	strfilename := fmt.Sprintf("config.json")
+	return strfilename
+}
